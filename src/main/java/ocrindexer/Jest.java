cf556dcdfd304da735f3557e602d9ecdb27715a2
Dennis Johansson <dennis@dnjo.net>
2018-12-26 02:01:24 +0100

    Execute functions outside VPC

--- src/main/java/ocrindexer/Jest.java
+++ src/main/java/ocrindexer/Jest.java
@@ -1,17 +1,26 @@
 package ocrindexer;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
 import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
 import com.amazonaws.services.simplesystemsmanagement.model.GetParameterResult;
+import com.google.common.base.Supplier;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
 import io.searchbox.core.Update;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import vc.inreach.aws.request.AWSSigner;
+import vc.inreach.aws.request.AWSSigningRequestInterceptor;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
@@ -46,7 +55,43 @@ public class Jest {
     private static JestClient buildJestClient() {
         AWSSimpleSystemsManagement ssmClient = AWSSimpleSystemsManagementClientBuilder.defaultClient();
         GetParameterResult ocrEsUrl = ssmClient.getParameter(new GetParameterRequest().withName("ocrEsUrl"));
-        JestClientFactory factory = new JestClientFactory();
+        final Supplier<LocalDateTime> clock = () -> LocalDateTime.now(ZoneOffset.UTC);
+        final AWSSigner awsSigner = new AWSSigner(new AWSCredentialsProvider() {
+            @Override
+            public AWSCredentials getCredentials() {
+                return new AWSCredentials() {
+                    @Override
+                    public String getAWSAccessKeyId() {
+                        return ssmClient.getParameter(new GetParameterRequest().withName("ocrEsAccessKey")).getParameter().getValue();
+                    }
+
+                    @Override
+                    public String getAWSSecretKey() {
+                        return ssmClient.getParameter(new GetParameterRequest().withName("ocrEsSecretKey")).getParameter().getValue();
+                    }
+                };
+            }
+
+            @Override
+            public void refresh() {
+
+            }
+        }, System.getenv("AWS_REGION"), "es", clock);
+
+        AWSSigningRequestInterceptor requestInterceptor = new AWSSigningRequestInterceptor(awsSigner);
+        JestClientFactory factory = new JestClientFactory() {
+            @Override
+            protected HttpClientBuilder configureHttpClient(HttpClientBuilder builder) {
+                builder.addInterceptorLast(requestInterceptor);
+                return builder;
+            }
+
+            @Override
+            protected HttpAsyncClientBuilder configureHttpClient(HttpAsyncClientBuilder builder) {
+                builder.addInterceptorLast(requestInterceptor);
+                return builder;
+            }
+        };
         factory.setHttpClientConfig(new HttpClientConfig
                 .Builder(ocrEsUrl.getParameter().getValue())
                 .build());
